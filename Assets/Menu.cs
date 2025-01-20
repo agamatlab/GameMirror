807c343708dad63324df099359b6581ead191e2c
agamatlab <icloudakberzade@gmail.com>
2025-01-20 00:18:08 -0500

    add: animation when switching scenes

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -8,13 +8,43 @@ public class Menu : MonoBehaviour
 {
     [SerializeField]
     Button StartButton;
-    // Start is called before the first frame update
+
+    [SerializeField]
+    GameObject transition;
+
+    [SerializeField]
+    Canvas Canvas;
+    // Start is called before t he first frame update
     void Start()
     {
+
+        Vector2 startPos = new Vector2(0, 0);
         StartButton.onClick.AddListener(() =>
         {
-            SceneManager.LoadScene(sceneName: "Level0");
+            int rowCount = Screen.width / 50;
+            int colCount = Screen.width /50 ;
+            for (int j = 0; j < colCount; j++)
+            {
+                for (int i = 0; i < rowCount;  i++)
+                {
+                    GameObject newBlock = Instantiate(transition);
+                    newBlock.transform.position = new Vector2(startPos.x + (i*70),startPos.y + (70*j));
+                    newBlock.transform.SetParent(Canvas.transform);
+                    newBlock.transform.localScale = Vector3.zero;
+                    LeanTween.scale(newBlock, Vector3.one*1f, 5f).setEaseOutElastic().setDelay(0.1f * j);
+                }
+
+            }
         });
+
+        StartCoroutine(CallingScene());
+    }
+
+    IEnumerator CallingScene()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(sceneName: "Level0");
+        
     }
 
     // Update is called once per frame
